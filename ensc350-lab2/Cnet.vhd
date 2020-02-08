002617a9d34aafee93a9da725c3563f917716b27
Choong Jin Ng <jin8383@gmail.com>
2020-02-07 20:04:06 -0800

    Lab2: Completed BookSkip Adder

--- ensc350-lab2/Cnet.vhd
+++ ensc350-lab2/Cnet.vhd
@@ -53,29 +53,60 @@ end architecture Ripple;
 
 architecture BookSkip of Cnet is
 	signal a : std_logic_vector(width downto 0);
-	signal b : std_logic;
+
+	signal interC : std_logic_vector(width/4 - 1 downto 0);
+	signal andOutput : std_logic_vector(width/4 - 1 downto 0);
+	signal skip : std_logic_vector(width/4 - 1 downto 0);
 begin
 	-- Assuming width %4 == 0
-	Recur: if (width > 4) generate
-		BookSkip1: entity Cnet(BookSkip) generic map(width/2)
-		port map (G(width/2 - 1 downto 0), P(width/2 - 1 downto 0), Cin, a(width/2 downto 0));
-		BookSkip2: entity Cnet(BookSkip) generic map(width/2)
-		port map (G(width - 1 downto width/2), P(width - 1 downto width/2), a(width/2), a(width downto width/2));
 
-		C(width downto 0) <= a(width downto 0);
-	End generate Recur;
+	-- a(0) <= Cin;
+	-- skip(0) <= Cin;
+	-- Cprop1: entity Work.Cprop port map( G(0), P(0), a(0), a(1));
+	-- Cprop2: entity Work.Cprop port map( G(1), P(1), a(1), a(2));
+    -- Cprop3: entity Work.Cprop port map( G(2), P(2), a(2), a(3));
+    -- Cprop4: entity Work.Cprop port map( G(3), P(3), a(3), interC(0));
+    -- Cand1: entity Work.and4 port map(P(0), P(1), P(2), P(3), andOutput(0));
+	-- Cskip1: entity Work.Cprop port map(interC(0), andOutput(0), skip(0), a(4));
+	
+	-- skip(1) <= a(4);    
+	-- Cprop5: entity Work.Cprop port map( G(4), P(4), a(4), a(5));
+	-- Cprop6: entity Work.Cprop port map( G(5), P(5), a(5), a(6));
+	-- Cprop7: entity Work.Cprop port map( G(6), P(6), a(6), a(7));
+    -- Cprop8: entity Work.Cprop port map( G(7), P(7), a(7), interC(1));
+    -- Cand2: entity Work.and4 port map(P(4), P(5), P(6), P(7), andOutput(1));
+	-- Cskip2: entity Work.Cprop port map(interC(1), andOutput(1), skip(1), a(8));
+	
+	-- skip(2) <= a(8);
+	-- Cprop9: entity Work.Cprop port map( G(8), P(8), a(8), a(9));
+	-- Cprop10: entity Work.Cprop port map( G(9), P(9), a(9), a(10));
+	-- Cprop11: entity Work.Cprop port map( G(10), P(10), a(10), a(11));
+    -- Cprop12: entity Work.Cprop port map( G(11), P(11), a(11), interC(2));
+    -- Cand3: entity Work.and4 port map(P(8), P(9), P(10), P(11), andOutput(2));
+	-- Cskip3: entity Work.Cprop port map(interC(2), andOutput(2), skip(2), a(12));
 
-	BaseCase: if (width = 4) generate
-		Cprop1: entity Work.Cprop port map(G(0), P(0), Cin, a(0));
-		Cprop2: entity Work.Cprop port map(G(1), P(1), a(0), a(1));
-		Cprop3: entity Work.Cprop port map(G(2), P(2), a(1), a(2));
-		Cprop4: entity Work.Cprop port map(G(3), P(3), a(2), a(3));
+	-- skip(3) <= a(12);
+	-- Cprop13: entity Work.Cprop port map( G(12), P(12), a(12), a(13));
+	-- Cprop14: entity Work.Cprop port map( G(13), P(13), a(13), a(14));
+	-- Cprop15: entity Work.Cprop port map( G(14), P(14), a(14), a(15));
+    -- Cprop16: entity Work.Cprop port map( G(15), P(15), a(15), interC(3));
+    -- Cand4: entity Work.and4 port map(P(12), P(13), P(14), P(15), andOutput(3));
+    -- Cskip4: entity Work.Cprop port map(interC(3), andOutput(3), skip(3), a(16));
 
-		Cand: entity Work.and4 port map(P(0), P(1), P(2), P(3), b);
+	a(0) <= Cin;
+	skip(0) <= Cin;
+	ForLoop: for i in 0 to width/4 - 1 generate
+		skip(i) <= a(i*4);
+		PropLoop: for j in 0 to 3 generate
+			Cpropj: entity Work.Cprop port map(G(i*4 + j), P(i*4 + j), a(i*4 + j), a(i*4 + j + 1));
+		End generate PropLoop;
 
-		Cpropout: entity Work.Cprop port map(a(3), b, Cin, C(4));
-		C(width - 1 downto 0) <= a(width - 1 downto 0);
-	End generate BaseCase;
+		CpropBefore: entity Work.Cprop port map(G(i*4 + 3), P(i*4 + 3), a(i*4 + 3), interC(i));
+		Cand: entity Work.and4 port map(P(i*4 + 0), P(i*4 + 1), P(i*4 + 2), P(i*4 + 3), andOutput(i));
+		Cskip4: entity Work.Cprop port map(interC(i), andOutput(i), skip(i), a(i*4 + 4));
+	End generate ForLoop;
+
+	C(width downto 0) <= a(width downto 0);
 end architecture BookSkip;
 
 
