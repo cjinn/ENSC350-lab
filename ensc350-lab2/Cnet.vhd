d3c97c59949b296bd97bd104cef32fe1fddf56e6
Choong Jin Ng <jin8383@gmail.com>
2020-02-07 16:38:35 -0800

    Working on Lab 2

--- ensc350-lab2/Cnet.vhd
+++ ensc350-lab2/Cnet.vhd
@@ -19,14 +19,48 @@ end entity Cnet;
 -- Students must Create the following Carry Network Architectures.
 -----------------------------------------------------------------------------
 architecture Ripple of Cnet is
+	signal tempCarry1 : std_logic; -- Don't read from output signal C
+	signal tempCarry2 : std_logic;
 begin
+	C(0) <= Cin;
+	C(8) <= Cin;
+	tempCarry1 <= Cin;
+	CpropGenerate: for i in 0 to width-1 generate
+		C(i) <= tempCarry1;
+		Cpropi: entity Work.Cprop port map(G(i), P(i), tempCarry1, tempCarry2);
+		tempCarry1 <= tempCarry2;
+	end generate CpropGenerate;
+	C(width) <= tempCarry2;
 end architecture Ripple;
 
 
 
 
 architecture BookSkip of Cnet is
+	signal a : std_logic_vector(width downto 0);
+	signal b : std_logic;
 begin
+	-- Assuming width %4 == 0
+	Recur: if (width > 4) generate
+		BookSkip1: entity Cnet(BookSkip) generic map(width/2)
+		port map (G(width/2 - 1 downto 0), P(width/2 - 1 downto 0), Cin, a(width/2 downto 0));
+		BookSkip2: entity Cnet(BookSkip) generic map(width/2)
+		port map (G(width - 1 downto width/2), P(width - 1 downto width/2), a(width/2), a(width downto width/2));
+
+		C(width downto 0) <= a(width downto 0);
+	End generate Recur;
+
+	BaseCase: if (width = 4) generate
+		Cprop1: entity Work.Cprop port map(G(0), P(0), Cin, a(0));
+		Cprop2: entity Work.Cprop port map(G(1), P(1), a(0), a(1));
+		Cprop3: entity Work.Cprop port map(G(2), P(2), a(1), a(2));
+		Cprop4: entity Work.Cprop port map(G(3), P(3), a(2), a(3));
+
+		Cand: entity Work.and4 port map(P(0), P(1), P(2), P(3), b);
+
+		Cpropout: entity Work.Cprop port map(a(3), b, Cin, C(4));
+		C(width - 1 downto 0) <= a(width - 1 downto 0);
+	End generate BaseCase;
 end architecture BookSkip;
 
 
@@ -40,5 +74,15 @@ end architecture GoodSkip;
 
 
 architecture BrentKung of Cnet is
+
 begin
+	-- Recur: if (width > 2) generate
+
+	-- 	InputPrep: for i in width - 1 downto 0 generate
+	-- 	End generate InputPrep;
+	-- End generate Recur;
+
+	-- BaseCase: if (width == 2) generate
+	-- 	Cprop: entity Work.Cprop port map (G(0), P(0), Cin, C);
+	-- End generate BaseCase;
 end architecture BrentKung;
